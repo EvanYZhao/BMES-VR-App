a7ac463be9bbd3bc5ed321caa2435338f25b8e4a
Evan Zhao <zevany16@gmail.com>
2024-01-17 10:49:57 -0800

    home screen bare bones
    * Renamed App.jsx to be Home.jsx
    * Styled home page to achieve parity with notion wireframe

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import App from "./screens/App.jsx";
+import Home from "./screens/Home.jsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
