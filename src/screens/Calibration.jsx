ad5db0d628daebe46e5022ebc99aa2a549ec9b3c
carissaott <105527439+carissaott@users.noreply.github.com>
2024-01-16 21:41:05 -0800

    added TODO's about backend thoughts

--- src/screens/Calibration.jsx
+++ src/screens/Calibration.jsx
@@ -1,14 +1,25 @@
 import { useState } from "react";
 import "../styles/Calibration.css"
+import Avatar from "../components/Avatar";
 
 function Calibration() {
+    const [bend, setBend] = useState(4);
+
   return (
     <>
     <div className={"instruction-box"}>
-    <h1>Calibration Page</h1>
-    <button className={"instruction-button"}>
-        this is as far as I can go
-    </button>
+        <h1>Calibration Page</h1>
+
+        <Avatar bend={bend} setBend={setBend} />
+
+        {/* instruction button: we will likely need to store the appropriate bend value to the backend
+            - TODO: figure out database structure
+            - TODO: set up API route to POST max/min bend calibration values to database
+            - TODO: establish various basic API routes (get, post, etc.) */}
+
+        <button className={"instruction-button"}>
+            this is as far as I can go
+        </button>
     </div>
     </>
   );
